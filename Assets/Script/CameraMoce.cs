c0ec4a5936134dffa8e5320cee8e64d43664376d
PPiR0604 <mfahraviisk@gmail.com>
2025-03-08 17:38:45 +0700

    Zoom in Camera (Unfinished)

--- Assets/Script/CameraMoce.cs
+++ Assets/Script/CameraMoce.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraMoce : MonoBehaviour
@@ -6,46 +7,42 @@ public class CameraMoce : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public Transform player;
-    public Camera mainCamera;
+    private float zoom;
+    private float zoomMultiplier = 4f;
+    private float minZoom = 3f;
+    private float maxZoom = 5f;
+    private float velocity = 0f;
+    private float smoothTime = 0.25f;
+    [SerializeField] private Camera cam;
+
     void Start()
     {
-        
+        zoom = cam.orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float y= transform.position.y;
+        
         if (transform.position.x < 13&& transform.position.x > -13)
         {
-        transform.position = new Vector3(player.position.x, y, transform.position.z);
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
         }
-        if (player.position.x < 13 && player.position.x > -13)
+        else if (player.position.x < 13 && player.position.x > -13)
         {
-            transform.position = new Vector3(player.position.x, y, transform.position.z);
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
         }
         
     }
 
     void FixedUpdate()
     {   
-        float y=0.1f;
-        if (Input.mouseScrollDelta.y <0  && mainCamera.orthographicSize <= 5f && mainCamera.orthographicSize >= 2.5f)
-        {   
-            mainCamera.orthographicSize += y;
-            Vector3 temp=transform.position;
-              float eq= mainCamera.orthographicSize;
-            mainCamera.transform.position = new Vector3(temp.x, 0.34f*eq, temp.z);
-        }
-        else if (Input.mouseScrollDelta.y > 0 && mainCamera.orthographicSize >= 2.5f  && mainCamera.orthographicSize <= 5f){
-            
-            mainCamera.orthographicSize -= y;
-            Vector3 temp=mainCamera.transform.position;
-            float eq= mainCamera.orthographicSize;
-            mainCamera.transform.position = new Vector3(temp.x, 0.34f*eq, temp.z);
-        }
-       
-        
+        //JANGAN DIOTAK ATIK!!
+        // float scroll = Input.GetAxis("Mouse ScrollWheel");
+        // zoom -= scroll * zoomMultiplier;
+        // zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        // cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+        // transform.position = new Vector3(transform.position.x, cam.orthographicSize*0.08f, transform.position.z);    
     }
    
 
