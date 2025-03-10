f5aecd7b67135992982707b1347cf0a9311d62d6
PPiR0604 <mfahraviisk@gmail.com>
2025-03-10 16:07:52 +0700

    CC Modif

--- Assets/Script/CC Manager.cs
+++ Assets/Script/CC Manager.cs
@@ -9,14 +9,29 @@ public class CCManager : MonoBehaviour
     [SerializeField] Button CH2;
     [SerializeField] Button CH3;
     [SerializeField] Button Kembali;
+    [SerializeField]private int[] Unlocked;
 
     void Start()
     {
-        
+        CH1.interactable=false;
+        CH2.interactable=false;
+        CH3.interactable=false;
+    }
+    void Update(){
+        if(Unlocked[0]==1){
+            CH1.interactable=true;
+        }
+        if(Unlocked[1]==1){
+            CH2.interactable=true;
+        }
+        if(Unlocked[2]==1){
+            CH3.interactable=true;
+        }
     }
 
     public void loadCH(string gameObject)
     {
         SceneManager.LoadScene(gameObject);
     }
+
 }
