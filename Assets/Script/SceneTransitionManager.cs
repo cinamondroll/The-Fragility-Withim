ef13190e50e022b2ddacda4af60e8b0edffe166d
misuaa <nurulinayahh19@student.ub.ac.id>
2025-03-21 13:44:44 +0700

    Scene Transition

---
+++ Assets/Script/SceneTransitionManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public static SceneTransitionManager instance { get; private set; }
+
+    [SerializeField] Image fadeImage;
+    [SerializeField] float fadeDuration = 1.0f;
+    
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            //START FADE
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    public void LoadSceneWithFade(string sceneName)
+    {
+        StartCoroutine(FadeAndLoad(sceneName));
+    }
+    
+    IEnumerator FadeAndLoad (string sceneNameTL)
+    {
+        yield return StartCoroutine(FadeIn());
+        SceneManager.LoadScene(sceneNameTL);
+    }
+    
+    IEnumerator FadeIn()
+    {
+        if (fadeImage != null)
+        {
+            Color color = fadeImage.color;
+            color.a = 0;
+
+            for (float i = 0; i < fadeDuration; i += Time.deltaTime * fadeDuration)
+            {
+                 color.a = Mathf.Lerp(0, 1, i / fadeDuration);
+                 fadeImage.color = color;
+                 yield return null; 
+            }
+
+            color.a = 1;
+            fadeImage.color = color;    
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        if (fadeImage != null)
+        {
+            Color color = fadeImage.color;
+            color.a = 1;
+
+            for (float i = 0; i < fadeDuration; i += Time.deltaTime * fadeDuration)
+            {
+                 color.a = Mathf.Lerp(1, 0, i / fadeDuration);
+                 fadeImage.color = color;
+                 yield return null; 
+            }
+
+            color.a = 0;
+            fadeImage.color = color;    
+        }
+    }
+
+}
